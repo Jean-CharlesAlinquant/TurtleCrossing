cb3593645d9cbc70e55038d89929f73d6ef149b2
Jean-CharlesAlinquant <jcalinquant@hotmail.com>
2024-01-03 13:58:10 +0000

    Adding car movements

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,30 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+TOP = 250
+BOTTOM = -250
+
+# Create cars that are 20px high by 40px wide that are randomly generated along the y-axis
+# and move to the left edge of the screen.
+# No cars should be generated in the top and bottom 50px of the screen
+# (think of it as a safe zone for our little turtle).
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        color = random.choice(COLORS)
+        self.color(color)
+        self.penup()
+        self.shapesize(1, 2)
+        start_y = random.randint(BOTTOM, TOP)
+        self.goto(250, start_y)
+
+    def move(self):
+        new_x = self.xcor() - MOVE_INCREMENT
+        self.goto(new_x, self.ycor())
+
+
--- main.py
+++ main.py
@@ -19,7 +19,18 @@ player = Player()
 screen.listen()
 screen.onkey(player.move_up, KEY_UP)
 
+loop = 0
+car_list = []
 game_is_on = True
 while game_is_on:
+    loop += 1
     time.sleep(0.1)
     screen.update()
+    if loop % 6 == 0:
+        car = CarManager()
+        car_list.append(car)
+
+    for car in car_list:
+        car.move()
+
+    # Detect collision between turtle and car
